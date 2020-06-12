ee1e11ed303afc01f4c4ba8d5e98c418cf0061b4
Yu-kiFujiwara <74yuki99@gmail.com>
2020-06-12 22:59:31 +0900

    20200612

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,13 @@
 class RoomsController < ApplicationController
   def show
-    @room = Room.find(params[:id]) #ルーム情報の取得
-    @message = Message.new #新規メッセージ投稿
-    @messages = @room.messages #このルームのメッセージを全て取得
-
-    @owner = User.find_by(id: @room.owner_id)
-    @player = User.find_by(id: @room.player_id)
+    @room = Room.find(params[:id])
+    if current_user.id == @room.player_id || current_user.id == @room.owner_id
+      @message = Message.new
+      @messages = @room.messages
+      @owner = User.find_by(id: @room.owner_id)
+      @player = User.find_by(id: @room.player_id)
+    else
+      redirect_to "/"
+    end
   end
 end
